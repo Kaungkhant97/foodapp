63436aec18a8b8ad7ec1b97c49267af512625e26
kaung khant <kkhantthu97@gmail.com>
2022-05-21 16:18:53 +1000

    initial commit

---
+++ app/src/main/java/com/interview/fatsecret/HomeActivity.kt
@@ -0,0 +1,18 @@
+package com.interview.fatsecret
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.interview.fatsecret.ui.main.HomeFragment
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, HomeFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/interview/fatsecret/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.interview.fatsecret.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
